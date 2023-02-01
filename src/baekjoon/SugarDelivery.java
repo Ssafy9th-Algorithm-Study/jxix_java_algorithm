e49b5280799cd00fb4dbcf0118f5b5dec0e19f3e
김지은 <jxixeun@gmail.com>
2023-02-01 15:13:13 +0900

    :technologist: feat: 백준 2839번 설탕 배달

---
+++ src/baekjoon/SugarDelivery.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SugarDelivery {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int ans = 0;
+        while (N % 5 != 0 && N > 0) {
+            N -= 3;
+            ans++;
+        }
+        if (N < 0) {
+            ans = -1;
+        }
+        else {
+            ans += (N/5);
+        }
+        System.out.println(ans);
+    }
+}
