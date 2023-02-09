77d66240392750c1ad5e1be5f673c2a415bf4c9b
김지은 <jxixeun@gmail.com>
2023-02-09 14:26:02 +0900

    :technologist: feat: 백준 2023번 신기한 소수

---
+++ src/baekjoon/BOJ2023.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2023 {
+
+    static StringBuilder sb = new StringBuilder();
+    static void makePrime(int num, int N, int cnt) {
+        if (cnt==N){
+            sb.append(num);
+            sb.append("\n");
+            return;
+        }
+        for (int i=1;i<10;i++){
+            if (isPrime(num*10 + i)){
+                makePrime(num*10 + i, N,cnt+1);
+            }
+        }
+    }
+    static boolean isPrime(int num) {
+        for (int i=2;i<=Math.sqrt(num);i++){
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] prime = {2, 3, 5, 7};
+        for (int i = 0; i < 4; i++) {
+            makePrime(prime[i], N, 1);
+        }
+        System.out.print(sb);
+    }
+}
