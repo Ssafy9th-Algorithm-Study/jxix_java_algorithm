3e3552a3585104a0293da75176dae2bd9ee5e141
김지은 <jxixeun@gmail.com>
2023-02-13 12:13:17 +0900

    :technologist: feat: 백준 1012번 유기농 배추

---
+++ src/baekjoon/BOJ1012.java
@@ -0,0 +1,55 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1012 {
+
+    static boolean [][] visited;
+    static int [][] map;
+    static int N, M;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < M && ny < N && (map[nx][ny] == 1) && !visited[nx][ny]){
+                dfs(nx, ny);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            visited = new boolean[M][N];
+            map = new int[M][N];
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+                map[X][Y] = 1;
+            }
+            int ans = 0;
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]){
+                        dfs(i, j);
+                        ans++;
+                    }
+                }
+            }
+            sb.append(ans).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
