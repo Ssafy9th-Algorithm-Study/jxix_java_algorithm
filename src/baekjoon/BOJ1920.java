6bcb495fe2691e5ef858551881f831e8457ce07c
김지은 <jxixeun@gmail.com>
2023-02-20 15:40:34 +0900

    :technologist: feat: 백준 1920번 수 찾기

---
+++ src/baekjoon/BOJ1920.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1920 {
+
+    static int[] arr;
+
+    private static int binarySearch(int start, int end, int target) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            sb.append(binarySearch(0, N - 1, Integer.parseInt(st.nextToken()))).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
